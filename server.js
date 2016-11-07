128eabba0523ee44f267bcb7e3c07d74bd036a47
carlej <carlej@oregonstate.edu>
2016-11-06 20:02:33 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,1 @@
-/*
- * Here, you should write a simple server to serve files statically.
- */
 
-var path = require('path');
-
-var staticDir = path.join(__dirname, 'public');
-var indexFilename = 'index.html';
-var notFoundFilename = '404.html';
-var port = process.env.PORT || 3000;
