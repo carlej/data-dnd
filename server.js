0c1d2813527252c1926bcf775c55a68e2eb462a8
carlej <carlej@oregonstate.edu>
2016-11-23 13:49:24 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,38 @@
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var app = express();
+var port = process.env.PORT || 3000;
+var infoJSON = path.join(__dirname,'json');
+var session = require(path.join(infoJSON,'session'));
+var campaign = require(path.join(infoJSON,'campaign'));
+var location = require(path.join(infoJSON,'location'));
+var npc = require(path.join(infoJSON,'npc'));
+var town = require(path.join(infoJSON,'town'));
 
+app.engine('handlebars',exphbs({ defaultLayout: 'main'}))
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/',function(req,res){
+  res.render('index-page',{
+    pageTitle:'Welcome"
+  });
+});
+
+app.get('/session',function(req,res){
+  res.render('session-page',{
+    pageTitle:'Session',
+    session:session
+  });
+});
+
+app.get('/session/:sess',function(req,res,next){
+  var sess = session[req.params.session];
+  
+  if(sess){
+    res.render('sess-page',
