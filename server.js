8cb7d9bcb7d25d984eee8c7f6f69805ee3e52f36
carlej <carlej@oregonstate.edu>
2016-11-25 16:22:41 -0800

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',function(req,res){
-  res.render('index-page',{
+  res.render('sessions-page',{
     pageTitle:'Welcome'
   });
 });
